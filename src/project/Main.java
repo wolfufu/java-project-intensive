5328f319db6fece40335d5cff7222c3363d5144b
ApsilonXi <volkov.volkova2000@mail.ru>
2025-10-29 22:48:13 +0300

    добавлено меню

---
+++ src/project/Main.java
@@ -0,0 +1,13 @@
+package project;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+
+        menu.run();
+        // вызовы методов
+    }
+
+
+}
