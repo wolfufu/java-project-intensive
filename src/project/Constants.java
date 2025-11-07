a8ae792c3d9992449dd38f4bf532e43d9c4a1c2d
german_kiselev <g.a.kiselev@gmail.com>
2025-11-07 15:13:54 +0300

    Рефакторинг системы валидации Car

--- src/project/Constants.java
+++ src/project/Constants.java
@@ -5,9 +5,9 @@ import java.time.LocalDate;
 public final class Constants {
     private Constants() {}
 
-    public static final int MIN_PROD_YEAR = 1885;
-    public static final int MAX_PROD_YEAR = LocalDate.now().getYear();
-    public static final int MIN_POWER = 20;
+    public static final int MIN_YEAR = 1885;
+    public static final int MAX_YEAR = LocalDate.now().getYear();
+    public static final int MIN_POWER = 50;
     public static final int MAX_POWER = 2000;
     public static final String[] MODELS_LIST = {"Toyota", "Tesla", "Audi", "Lada", "BMW", "WV"};
     public static final String DEFAULT_FILENAME = "cars.txt";
